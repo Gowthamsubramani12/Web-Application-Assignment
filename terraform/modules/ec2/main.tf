245db91e6afead85bb42c67bf01d21840f41123f
DevOps_ho1 <devops.ho1@isolve.global>
2026-05-26 13:23:01 +0530

    update ecr changes

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -9,8 +9,9 @@ data "aws_ami" "amazon_linux_2023" {
 }
 
 resource "aws_ecr_repository" "app" {
-  name                 = "${var.project_name}-repo-${var.environment}"
+  name                 = "webapp-production"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
